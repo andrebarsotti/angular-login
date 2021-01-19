5563f6fc64eb7613877672e894a13387fe1e77a7
Andre Barsotti <andre.barsotti@accelera.com.br>
2021-01-19 11:08:13 -0300

    Ajustes no layout.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ServicesComponent } from './components/services/services.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'services',
+    component: ServicesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
